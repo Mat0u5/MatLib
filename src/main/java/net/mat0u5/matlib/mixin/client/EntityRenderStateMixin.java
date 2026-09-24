c30cfefbf57709d2d69b9b2e01b6d6128c0aa7ad
Mat0u5 <matous.danko@gmail.com>
2026-09-24 12:25:26 +0200

    Render Utils

---
+++ src/main/java/net/mat0u5/matlib/mixin/client/EntityRenderStateMixin.java
@@ -0,0 +1,48 @@
+package net.mat0u5.matlib.mixin.client;
+//? if <= 1.21 {
+/*import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import dev.kikugie.fletching_table.annotation.MixinEnvironment;
+
+@Mixin(value = MinecraftServer.class)
+@MixinEnvironment(type = MixinEnvironment.Env.CLIENT)
+public class EntityRenderStateMixin {
+    //Empty class to avoid mixin errors
+}
+*///?} else {
+import dev.kikugie.fletching_table.annotation.MixinEnvironment;
+import net.mat0u5.matlib.client.util.interfaces.IEntityRenderState;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(value = EntityRenderState.class, priority = 1)
+@MixinEnvironment(type = MixinEnvironment.Env.CLIENT)
+public class EntityRenderStateMixin implements IEntityRenderState {
+    @Unique
+    Entity ml$entity = null;
+    @Unique
+    float ml$tickProgress = 0;
+
+    @Unique
+    @Nullable
+    @Override
+    public Entity ml$getEntity() {
+        return ml$entity;
+    }
+
+    @Unique
+    @Override
+    public float ml$getTickProgress() {
+        return ml$tickProgress;
+    }
+
+    @Unique
+    @Override
+    public void ml$update(Entity entity, float tickProgress) {
+        ml$entity = entity;
+    }
+}
+//?}
